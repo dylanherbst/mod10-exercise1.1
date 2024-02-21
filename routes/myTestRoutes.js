9d94aed08fcf3d745cd709abb3d273165f3594fa
dylanherbst <dylan.herbstwork@gmail.com>
2024-02-21 14:32:14 +0800

    api add

--- routes/myTestRoutes.js
+++ routes/myTestRoutes.js
@@ -1,10 +1,21 @@
-
 const express = require('express');
 const router = express.Router();
 
+let products = () => {
+    return fetch('https://fakestoreapi.com/products/1')
+            .then(res => res.json())
+            .then(json => {
+                console.log(json);
+                return json; // Return the product data
+            });
+};
+
 router.get('/test', (req, res) => {
-res.send('Hello World Route! NodeMon Works here too')
-})
+    products().then(product => {
+        // Send the product data in the response
+        res.send(`Hello World Route! NodeMon Works here too ${JSON.stringify(product)}`);
+    });
+});
 router.get('/test2', (req, res) => {
 res.send('Second test')
 })
